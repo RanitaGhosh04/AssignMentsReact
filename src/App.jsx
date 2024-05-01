c147190a1dc63e9ded5c030aaaf6143f323e2a03
Ranita Ghosh <ranitaghosh04@gmail.com>
2024-05-01 10:13:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return <>
+   <h1>This is h1 tag</h1>
+
+   <div>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dicta in pariatur, reprehenderit repellendus recusandae aliquam excepturi nobis fuga ipsam tenetur nihil libero adipisci, quia, sunt esse! Accusamus, pariatur architecto.</p>
+   </div>
+
+   <div>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium dolorem ullam sit illo ab earum modi sed corporis voluptas ad odio rem tempora nisi itaque rerum veniam provident, facilis voluptatibus!
+    </p>
+
+    <div className='flex gap-5 justify-center'>
+    <a href=''>facebook</a>
+    <a href=''>instagram</a>
+    <a href=''>snapchat</a>
+    </div>
+    
+   </div>
+
+   <div>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste sit exercitationem voluptate, eveniet corporis ducimus ipsa? Dignissimos saepe non nulla possimus, necessitatibus quidem. Quisquam commodi ipsam ratione rem temporibus distinctio.
+    </p>
+   </div>
+  </>
+}
+
+export default App
